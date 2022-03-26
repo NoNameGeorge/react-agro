c51013a344d53b609a5563555b194c2376366ce7
George <73098841+NoNameGeorge@users.noreply.github.com>
2022-03-26 15:03:45 +0300

    first commit

---
+++ src/redux/reducers/users.ts
@@ -0,0 +1,13 @@
+const initialState = {
+    isAuth: false
+};
+
+const users = (state = initialState, action) => {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
+
+export default users;
