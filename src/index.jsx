c2eaf5ca9beab4591f552a5db70a7746389f5a2c
George <73098841+NoNameGeorge@users.noreply.github.com>
2022-03-28 21:06:25 +0300

    deplo vercel

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 import './scss/app.scss' 
